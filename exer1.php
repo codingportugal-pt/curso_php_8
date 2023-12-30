38be4c88f17802b04f04ab1c28fd1501c11dbe07
codingportugal-pt <codingportugal@gmail.com>
2023-12-30 17:24:00 +0000

    first commit

---
+++ exer1.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = 1000;
+$b = $a / 2;
+$c = 100;
+$d = 3 * $c;
+$e = 20;
+$x = 100;
+$y = 250;
+$z = $x + $y;
+$nome = "Pedro";
+$sobrenome = "Paulo";
+
+
+echo $b;
+echo'<br>-------------------<br>';
+echo $d;
+echo'<br>-------------------<br>';
+echo $e++;
+echo'<br>-------------------<br>';
+echo $e;
+echo'<br>-------------------<br>';
+echo ++$z;
+echo'<br>-------------------<br>';
+echo $x.' '.$y;
+echo'<br>-------------------<br>';
+echo $nome.' '.$sobrenome; 
+echo'<br>-------------------<br>';
